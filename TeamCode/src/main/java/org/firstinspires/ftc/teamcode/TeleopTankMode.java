bca91a69257d23b10cc7b2f025c0845a51c98ceb
Stuti Arora <arorastu100@gmail.com>
2016-11-19 10:34:02 -0600

    added loader code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTankMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTankMode.java
@@ -11,6 +11,10 @@ public class TeleopTankMode extends OpMode {
 
     DcMotor leftMotor;
     DcMotor rightMotor;
+    DcMotor loaderMotor;
+
+
+    float loaderPower = 0;
 
     @Override
     public void init() {
@@ -19,8 +23,12 @@ public class TeleopTankMode extends OpMode {
         //get references to the motors from the hardware map
         leftMotor = hardwareMap.dcMotor.get("left_drive");
         rightMotor = hardwareMap.dcMotor.get("right_drive");
+        loaderMotor = hardwareMap.dcMotor.get("loader");
+
         //reverse the right motor
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+
 
 
     }
@@ -31,8 +39,20 @@ public class TeleopTankMode extends OpMode {
         float leftY = -gamepad1.left_stick_y;
         float rightY = -gamepad1.right_stick_y;
 
+        if (gamepad1.left_bumper){
+            loaderPower = 1;
+        }else{
+            loaderPower = 0;
+        }
+
+
+
+
+
         leftMotor.setPower(leftY);
         rightMotor.setPower(rightY);
+        loaderMotor.setPower(loaderPower);
+
     }
 }
 
