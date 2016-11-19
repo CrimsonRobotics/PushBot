ff8038dd0fbf1a2fcc3a29012c5243f9bf78b55e
Stuti Arora <arorastu100@gmail.com>
2016-11-19 11:42:24 -0600

    added servo code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTankMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTankMode.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "TeleopTankMode", group = "Sensor")
 
@@ -13,6 +14,16 @@ public class TeleopTankMode extends OpMode {
     DcMotor rightMotor;
     DcMotor loaderMotor;
     DcMotor shooterMotor;
+
+    Servo upLeftServo;
+    Servo dnLeftServo;
+    Servo upRightServo;
+    Servo dnRightServo;
+
+    double closePosition = 0.2;
+    double openPosition = 0.4;
+
+
 
     float loaderPower = 0;
     float shooterPower = 0;
@@ -26,6 +37,12 @@ public class TeleopTankMode extends OpMode {
         rightMotor = hardwareMap.dcMotor.get("right_drive");
         loaderMotor = hardwareMap.dcMotor.get("loader");
         shooterMotor = hardwareMap.dcMotor.get("shooter");
+
+        upLeftServo = hardwareMap.servo.get("upLeftServo");
+        dnLeftServo = hardwareMap.servo.get("dnLeftServo");
+        upRightServo = hardwareMap.servo.get("upRightServo");
+        dnRightServo = hardwareMap.servo.get("dnRightServo");
+
 
         //reverse the right motor
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
@@ -54,7 +71,19 @@ public class TeleopTankMode extends OpMode {
         }
 
 
-
+        if(gamepad1.x) {
+            upLeftServo.setPosition(openPosition);
+            upRightServo.setPosition(openPosition);
+            dnLeftServo.setPosition(openPosition);
+            dnRightServo.setPosition(openPosition);
+        }
+        //Move servo 1 to the down position when a button is pressed
+        if(gamepad1.b) {
+            upLeftServo.setPosition(closePosition);
+            upRightServo.setPosition(closePosition);
+            dnLeftServo.setPosition(closePosition);
+            dnRightServo.setPosition(closePosition);
+        }
 
 
         leftMotor.setPower(leftY);
